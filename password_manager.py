7babe8c41400ce00b7d3d46b1c99c3d35e85cb1d
AhmadAlkhooly08 <ahmadalkhooly08@gmail.com>
2024-02-28 23:32:55 +0300

    i come back to html from 2 years

--- password_manager.py
+++ password_manager.py
@@ -1,24 +1,50 @@
+from cryptography.fernet import Fernet
+
+'''
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)'''
+
+
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+
+key = load_key()
+fer = Fernet(key)
+
 
 def view():
-  with open('password.txt','r')as f:
-   for line in f.readlines():
-    data = line.rstrip()
-   if len(data.split()) >= 2:
-    user, code = data.split()
-    print(f'user: {user} password: {code} ')
+    with open('passwords.txt', 'r') as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            user, passw = data.split("|")
+            print("User:", user, "| Password:",
+                  fer.decrypt(passw.encode()).decode())
+
+
 def add():
- name = input('add your Account name please:')
- pwd = input('add your password please:')
- with open('password.txt','a')as f: 
-  f.write(name + ' ' + pwd +'\n')
+    name = input('Account Name: ')
+    pwd = input("Password: ")
+
+    with open('passwords.txt', 'a') as f:
+        f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
+
 
 while True:
- mode=input('Would you like to add a new password or view existing ones (view, add) or press q to quit:').lower()
- if mode == 'q':
-  break
- if mode == 'view':
-  view()
- elif mode == 'add':
-  add()
- else:
-  print('invalid mode')
+    mode = input(
+        "Would you like to add a new password or view existing ones (view, add), press q to quit? ").lower()
+    if mode == "q":
+        break
+
+    if mode == "view":
+        view()
+    elif mode == "add":
+        add()
+    else:
+        print("Invalid mode.")
+        continue
